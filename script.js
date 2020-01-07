e751c5f8355f6e05ac571ff378d8f3245c157373
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-07 12:07:54 -0800

    reduced total amount of time

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ console.log(questions);
 // starts game
 function startQuiz() {
     // sets timer start at 75 seconds
-    timerSecs = 75;
+    timerSecs = 40;
     timerDisplay.textContent = timerSecs;
 
     // starts countdown
