05fe103d3abd70c3d6dcefa8e2fedc8a8fed94b8
Marti <52050778+github-marti@users.noreply.github.com>
2020-01-07 12:24:30 -0800

    added question to quiz

--- questions.js
+++ questions.js
@@ -1,29 +1,33 @@
 let questions = [
-    {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
-    },
-    {
-      title: "The condition in an if / else statement is enclosed within ____.",
-      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
-    },
-    {
-     title: "What is the name of the statement that is used to exit or end a loop?",
-     choices: ["falter statement", "conditional statement", "break statement", "close statement"],
-     answer: "break statement"
-    },
-    {
-     title: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
-     choices: ["scope", "restriction", "range", "output level"],
-     answer: "scope"
-    },
-    {
-     title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
-     choices: ["conditional loop", "for loop", "else loop", "while loop"],
-     answer: "while loop"
-    }
-    ]
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "What is the name of the statement that is used to exit or end a loop?",
+    choices: ["falter statement", "conditional statement", "break statement", "close statement"],
+    answer: "break statement"
+  },
+  {
+    title: "What is the element called that is used to describe the set of variables, objects, and functions you explicitly have access to?",
+    choices: ["scope", "restriction", "range", "output level"],
+    answer: "scope"
+  },
+  {
+    title: "What is the type of loop that continues through a block of code as long as the specified condition remains TRUE?",
+    choices: ["conditional loop", "for loop", "else loop", "while loop"],
+    answer: "while loop"
+  },
+  {
+    title: "Which of the following is correct JavaScript syntax to manipulate the DOM?",
+    choices: ["document.getElement('p').innerHTML = 'foo'", "#demo.innerHTML = 'foo'", "document.getElementById('demo').innerHTML = 'foo'", "document.getElementByName('p').innerHTML = 'foo'"],
+    answer: "document.getElementById('demo').innerHTML = 'foo'"
+  }
+]
 
-  
